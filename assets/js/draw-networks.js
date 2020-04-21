f81f418a41c6ca52d9c6f024ef4cf35f69d149d0
clintonwxy <cwxy.clinton@gmail.com>
2020-04-21 16:40:00 +0800

    passing over development of graph

--- assets/js/draw-networks.js
+++ assets/js/draw-networks.js
@@ -1,6 +1,6 @@
 var canvas = d3.select("#networks")
     .append("canvas")
-    .attr("width", 800)
+    .attr("width", 600)
     .attr("height", 600),
     width = canvas.attr("width"),
     height = canvas.attr("height"),
@@ -16,8 +16,8 @@ var canvas = d3.select("#networks")
         .force("charge", d3.forceManyBody()
             .strength(-20))
         .force("link", d3.forceLink()
-            .id(function (d) { return d.name; }));
-
+            .id(function (d) { return d.name; })),
+    nested = 0;
 
 d3.json("assets/js/graphFile.json", function (err, graph) {
     if (err) throw err;
@@ -59,8 +59,74 @@ function dragstarted() {
     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
     d3.event.subject.fx = d3.event.subject.x;
     d3.event.subject.fy = d3.event.subject.y;
-    console.log(d3.event.subject);
-    console.log("hi") // Edit to print school name
+
+    // Appending Name
+    var currentNode = d3.event.subject;
+    d3.select("#networkschoolname").text("School: " + currentNode.name)
+
+    if (nested == 0) {
+        // Subgraph
+        d3.json("assets/js/graphFile.json", function (data) {
+
+            output_nodes = data.nodes.filter(function (d) {
+                return d.cluster == currentNode.cluster
+            })
+
+            output_links = data.links.filter(function (d) { //not working properly, need to refilter
+                return d.source in output_nodes
+            })
+
+            filtered_output = {
+                "nodes": output_nodes,
+                "links": output_links
+            };
+
+            console.log(filtered_output)
+
+            var subcanvas = d3.select("#networks-subgraph")
+                .append("canvas")
+                .attr("width", 600)
+                .attr("height", 600),
+                width = subcanvas.attr("width"),
+                height = subcanvas.attr("height"),
+                ctx2 = subcanvas.node().getContext("2d"),
+                r = 3,
+                color = d3.scaleOrdinal() // D3 Version 4
+                    .domain(["PRIMARY", "SECONDARY", "JUNIOR COLLEGE", "MIXED LEVEL", "CENTRALISED INSTITUTE"])
+                    .range(["red", "blue", "green", "yellow", "pink"]),
+                simulation2 = d3.forceSimulation()
+                    .force("x", d3.forceX(width / 2))
+                    .force("y", d3.forceY(height / 2))
+                    .force("collide", d3.forceCollide(r + 1))
+                    .force("charge", d3.forceManyBody()
+                        .strength(-20))
+                    .force("link", d3.forceLink()
+                        .id(function (d) { return d.name; }));
+
+            simulation2.nodes(filtered_output.nodes);
+            simulation2.force("link")
+                .links(filtered_output.links);
+            simulation2.on("tick", function () {
+                ctx2.clearRect(0, 0, width, height);
+
+                ctx2.beginPath();
+                ctx2.globalAlpha = 0.5;
+                ctx2.strokeStyle = "#aaa";
+                filtered_output.links.forEach(function (l) {
+                    ctx2.moveTo(l.source.x, l.source.y);
+                    ctx2.lineTo(l.target.x, l.target.y);
+                });
+                ctx2.stroke();
+
+
+                ctx2.globalAlpha = 1.0;
+                filtered_output.nodes.forEach(drawNode);
+            });
+        });
+
+        nested = 1
+    }
+
 }
 
 function dragged() {
@@ -78,7 +144,8 @@ function dragended() {
 
 function drawNode(d) {
     ctx.beginPath();
-    ctx.fillStyle = color(d.party);
+    //ctx.fillText(d.name, d.x + 10, d.y + 3);
+    ctx.fillStyle = color(d.level);
     ctx.moveTo(d.x, d.y);
     ctx.arc(d.x, d.y, r, 0, Math.PI * 2);
     ctx.fill();
